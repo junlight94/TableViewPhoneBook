f542de3c8f444b70114d7d1a296b845fbdbd6173
junyoung <junlight94@gmail.com>
2022-03-28 15:32:21 +0900

    start

---
+++ TableViewPhoneBook/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  TableViewPhoneBook
+//
+//  Created by 이준영 on 2022/03/28.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
